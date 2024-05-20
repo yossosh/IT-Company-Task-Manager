f393656be9adf5063803e24109f93825280a105c
Sofiia <sonagm9@gmail.com>
2024-05-20 11:38:26 +0300

    create a home function in views.py

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Position, TaskType, Worker, Task
+
+
+def home(request) -> HttpResponse:
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+    num_tasks = Task.objects.count()
+    num_task_types = TaskType.objects.count()
+    return render(
+        request,
+        "manager/home.html",
+        {
+            "num_workers": num_workers,
+            "num_positions": num_positions,
+            "num_tasks": num_tasks,
+            "num_task_types": num_task_types,
+        },
+    )
