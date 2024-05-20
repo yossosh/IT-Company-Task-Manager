2a1c2177a7fedb5b303ec40f3abafc02ac170970
Sofiia <sonagm9@gmail.com>
2024-05-20 12:05:26 +0300

    update home function

--- manager/views.py
+++ manager/views.py
@@ -5,17 +5,15 @@ from manager.models import Position, TaskType, Worker, Task
 
 
 def home(request) -> HttpResponse:
-    num_workers = Worker.objects.count()
-    num_positions = Position.objects.count()
-    num_tasks = Task.objects.count()
-    num_task_types = TaskType.objects.count()
+    """View function for the home page of the site."""
+
     return render(
         request,
         "manager/home.html",
         {
-            "num_workers": num_workers,
-            "num_positions": num_positions,
-            "num_tasks": num_tasks,
-            "num_task_types": num_task_types,
+            "num_workers": Worker.objects.count(),
+            "num_positions": Position.objects.count(),
+            "num_tasks": Task.objects.count(),
+            "num_task_types": TaskType.objects.count(),
         },
     )
