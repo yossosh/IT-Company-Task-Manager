cf4c843834bb3d80a356127b0c1bd91362cdf360
Sofiia <sonagm9@gmail.com>
2024-07-19 20:01:35 +0200

    staticfiles managed

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -131,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = "staticfiles"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
